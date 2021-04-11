16ee09526fd99c8cac4cdfd2fc6dad63da336feb
Shashank Shet <shashankshet@gmail.com>
2021-04-11 11:35:36 +0530

    Replace the default init file with this on when on Colab

---
+++ colab_init.py
@@ -0,0 +1,27 @@
+DATASET_PATH = "./drive/MyDrive/cs\ 753/dataset-preprocessed/"
+
+MODEL_SAVE_PATH = "./drive/MyDrive/cs\ 753/"
+
+INSTRUMENTS = [
+    "Bansuri",
+    "Shehnai",
+    "Santoor",
+    "Sarod",
+    "Sitar"
+]
+
+INSTRUMENT_LABELS = {
+    "Bansuri" : 0,
+    "Shehnai" : 1,
+    "Santoor" : 2,
+    "Sarod"   : 3,
+    "Sitar"   : 4
+}
+
+NUM_FILES_PER_INSTRUMENT = [
+    728,
+    302,
+    479,
+    456,
+    1291
+]
